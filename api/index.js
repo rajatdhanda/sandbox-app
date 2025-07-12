a896ab649d847741c85ec4bc8f293cfd17822bf1
rajatdhanda <dhanda.rajat@gmail.com>
2025-07-12 08:54:20 +0530

    Configure Vercel to build Expo web app on deployment

---
+++ api/index.js
@@ -0,0 +1,14 @@
+const path = require('path');
+const express = require('express');
+
+const app = express();
+
+// Serve static files from the dist directory
+app.use(express.static(path.join(__dirname, '../dist')));
+
+// Handle client-side routing
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../dist/index.html'));
+});
+
+module.exports = app;
